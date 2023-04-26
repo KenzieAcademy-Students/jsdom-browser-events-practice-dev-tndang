68bea60fb01a0b2a964969a7d67415847b8544d5
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-04-25 20:14:40 -0400

    Completed step 3, and now you can move the box with the keyboard arrow keys.

--- code.js
+++ code.js
@@ -6,7 +6,24 @@ let boxTop = 200;
 let boxLeft = 200;
 
 let onKeyPress = function(event) {
-    console.log("You Pressed: " + event.key);
+    if (event.key === "ArrowDown") {
+        boxTop += 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowUp") {
+        boxTop -= 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowLeft") {
+        boxLeft -= 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowRight") {
+        boxLeft += 10;
+        console.log("You Pressed: " + event.key);
+    } else {
+        console.log("That key is invalid, please press an arrow key.");
+    }
+
+    document.getElementById("box").style.top = boxTop + "px";
+    document.getElementById("box").style.left = boxLeft + "px";
 }
 
 document.addEventListener("keydown", onKeyPress);
