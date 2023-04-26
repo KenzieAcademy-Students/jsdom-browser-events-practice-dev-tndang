57a549e5c3454c6d650d3b45a0b03e47569dcea8
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-26 00:49:31 +0000

    deploy: 1cd1e62c808398bc32d95de8d25d4de26ec63c52

--- code.js
+++ code.js
@@ -2,3 +2,42 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+let boxTop = 200;
+let boxLeft = 200;
+
+let resetButton = document.getElementById("resetButton");
+
+let resetCube = function() {
+    boxTop = 200;
+    boxLeft = 200;
+
+    document.getElementById("box").style.top = boxTop + "px";
+    document.getElementById("box").style.left = boxLeft + "px";
+
+    console.log("The cube has been reset!")
+} 
+
+resetButton.addEventListener("click", resetCube);
+
+let onKeyPress = function(event) {
+    if (event.key === "ArrowDown") {
+        boxTop += 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowUp") {
+        boxTop -= 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowLeft") {
+        boxLeft -= 10;
+        console.log("You Pressed: " + event.key);
+    } else if (event.key === "ArrowRight") {
+        boxLeft += 10;
+        console.log("You Pressed: " + event.key);
+    } else {
+        console.log("That key is invalid, please press an arrow key.");
+    }
+
+    document.getElementById("box").style.top = boxTop + "px";
+    document.getElementById("box").style.left = boxLeft + "px";
+}
+
+document.addEventListener("keydown", onKeyPress);
